2bf59c7575343899ea4a82c8d9d728a37ebff1f4
Yossef Rostaqi <yr@yossef.dev>
2020-11-04 19:39:24 +0100

    📦 Add Webpack bundler support

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: 'src/index.html',
+					to: 'index.html'
+				},
+			]
+		})
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000
+	}
+};
