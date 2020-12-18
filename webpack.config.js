569ef13df41b89c6697c369b106b69accd80daa9
Yossef Rostaqi <yr@yossef.dev>
2020-12-18 16:29:18 +0100

    🔧 Add support to ES6/ES2015 via babel

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,19 @@ module.exports = {
 		contentBase: path.join(__dirname, 'dist'),
 		compress: true,
 		port: 9000
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
 	}
 };
