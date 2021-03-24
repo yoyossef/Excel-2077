e01ef3fdd1dcea380846e55ccf97a7761b0260b7
Aossywa <twouix@hotmail.fr>
2021-03-24 10:57:23 +0100

    :art: increase zoom speed and disable orange hover

--- src/components/cell_component/cell-component.js
+++ src/components/cell_component/cell-component.js
@@ -28,30 +28,30 @@ AFRAME.registerComponent('cell', {
 
         this.el.setAttribute('animation__mouseenter', { property: 'position',
                                             to: {x:this.el.object3D.position.x, y: this.el.object3D.position.y, z: 0.1},
-                                            dur: 250,
+                                            dur: 150,
                                             startEvents: 'mouseenter'}); 
 
         this.el.setAttribute('animation__mouseenter2', { property: 'scale',
                                             to: {x:1.1, y:1.1, z: 1},
-                                            dur: 250,
+                                            dur: 150,
                                             startEvents: 'mouseenter'});  
 
         this.el.setAttribute('animation__mouseleave', { property: 'position',
                                             to: {x:this.el.object3D.position.x, y: this.el.object3D.position.y, z: 0},
-                                            dur: 250,
+                                            dur: 150,
                                             startEvents: 'mouseleave'});  
 
         this.el.setAttribute('animation__mouseleave2', { property: 'scale',
                                             to: {x:1, y: 1, z: 1},
-                                            dur: 250,
+                                            dur: 150,
                                             startEvents: 'mouseleave'});                                      
     },
     events: {
         mouseenter: function (evt) {
-            this.el.setAttribute('material',{color: '#FF5100'});
+            //this.el.setAttribute('material',{color: '#FF5100'});
         },
         mouseleave: function (evt) {
-            this.el.setAttribute('material',{color: this.data.bgColor});
+            //this.el.setAttribute('material',{color: this.data.bgColor});
         },
         click : function (evt) {
             switch(ToolController.toolMode){
