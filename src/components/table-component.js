aac2d9566160d2d93096de1f666a59d0dbb4e4b2
Aossywa <twouix@hotmail.fr>
2021-03-24 09:35:29 +0100

    :zap: improve table component

--- src/components/table-component.js
+++ src/components/table-component.js
@@ -1,13 +1,14 @@
 AFRAME.registerComponent('table', {
     schema: {
         cellHauteur:    {type: 'number'},
+        dataMatrix:     {type: 'array'}
     },
 
     multiple: true,
 
     init: function () {
-        var data = [
-            ["Chiffre", "Mots", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom"],
+        this.data.dataMatrix = [
+            ["Chiffre", "Mots", "Prenom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom", "Prénom"],
             [1, "Aubergine", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan"],
             [1, "Aubergine", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan"],
             [1, "Aubergine", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan"],
@@ -38,66 +39,65 @@ AFRAME.registerComponent('table', {
             [1, "Aubergine", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan"],
             [1, "Aubergine", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan"],
             [1, "Aubergine", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan", "Dylan"],
-          ];
+        ];
+
+        var BgColor;
+        var color1=""
+        var color2=""
+    
+        for(var i = 0; i < this.data.dataMatrix.length; i++) {
+            var ligne = this.data.dataMatrix[i];
+            var message = Math.abs(i) - 1;
         
-          var tableElement =  document.getElementById('table');
-          var BgColor;
-          var color1=""
-          var color2=""
-        
-          for(var i = 0; i < data.length; i++) {
-              var ligne = data[i];
-              var message = Math.abs(i) - 1;
-        
-              if (message=='-1')
+            if (message=='-1')
                 message=' '
+
+            color1="#778899"
+            color2="#708090"
         
-              color1="#778899"
-              color2="#708090"
-        
-              if (i%2)
+            if (i%2)
                 BgColor = color1;
-              else
+            else
                 BgColor = color2;
-              
-              var newCell = document.createElement("a-entity");
-              newCell.setAttribute('cell', 'message: '+ message +'; color: #000000; bgColor: '+BgColor);
-              newCell.setAttribute('position', -1 +' '+ -(i-1)*this.data.cellHauteur +' 0');
-              newCell.setAttribute('id', -1 +';'+ i-1);
-              tableElement.appendChild(newCell);
+            // crée un nouvel élément a-entity 
+            var newCell = document.createElement("a-entity");
+            newCell.setAttribute('cell', 'message: '+ message +'; color: #000000; bgColor: '+BgColor);
+            newCell.setAttribute('position', -1 +' '+ -(i-1)*this.data.cellHauteur +' 0');
+            newCell.setAttribute('id', -1 +';'+ i-1);
+            this.el.appendChild(newCell);
         
-              if (i==0){
+            if (i==0){
                 color1="#778899"
                 color2="#708090"
-              }else{
+            }else{
                 color1="#DCDCDC"
                 color2="#D3D3D3"
-              }
+            }
         
-              for(var j = 0; j < ligne.length; j++) {
+            for(var j = 0; j < ligne.length; j++) {
         
                 if (i%2) {
-                  if (j%2)
+                if (j%2)
                     BgColor = color1;
-                  else
+                else
                     BgColor = color2;
                 } else {
-                  if (j%2)
+                if (j%2)
                     BgColor = color2; 
-                  else
+                else
                     BgColor = color1;  
                 }
         
-                // crée un nouvel élément div
+                // crée un nouvel élément a-entity
                 var newCell = document.createElement("a-entity");
                 newCell.setAttribute('cell', 'message:'+ ligne[j] +'; color: #000000; bgColor: '+BgColor);
-                newCell.setAttribute('position', j +' '+ -(i-1)*0.22 +' 0');
+                newCell.setAttribute('position', j +' '+ -(i-1)*this.data.cellHauteur +' 0');
                 newCell.setAttribute('id', j-1 +';'+ i-1);
         
                 // ajoute le nouvel élément créé et son contenu dans le DOM
-                tableElement.appendChild(newCell);
-              }
-          }
+                this.el.appendChild(newCell);
+            }
+        }
     
     }
 
