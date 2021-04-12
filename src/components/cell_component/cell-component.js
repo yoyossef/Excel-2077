3ad2acba5fe4d82a1afe43e6cfa4188aca6060b2
Aossywa <twouix@hotmail.fr>
2021-04-12 17:10:52 +0200

    improving data display

--- src/components/cell_component/cell-component.js
+++ src/components/cell_component/cell-component.js
@@ -7,9 +7,16 @@ AFRAME.registerComponent('cell', {
         bgColor:    {type: 'color', default: '#FF0000'},
         cellH :     {type: 'number', default: 0.22},
         type :      {type: 'string'},
+        position :  {type: 'array'},
+        angle :     {type: 'number'}
 
     },
     init: function () {
+        var mode = 3;
+        this.data.position=[this.el.object3D.position.x,this.el.object3D.position.y,this.el.object3D.position.z]
+       
+        var zoomedPos= new Array(3);
+
         this.mesh = new THREE.Mesh();
         this.el.setObject3D('mesh', this.mesh);
         this.el.setAttribute('class', 'links');
@@ -27,26 +34,50 @@ AFRAME.registerComponent('cell', {
         this.el.setAttribute('material', {  color: this.data.bgColor,
                                             shader: 'flat',
                                             visible: true});
+                                
+        // Position animation
+        switch (mode){
+            case 1: //wall
+                zoomedPos = [this.data.position[0],this.data.position[1],this.data.position[2]+0.1]    
+                break;
+            case 2: //cylinder
+            case 3: 
+                var offset;
+                var radius =  this.el.parentNode.getAttribute('table').radius-0.1;
+                if (mode == 2){
+                    offset = 0;
+                    radius *= 2
+                }else{
+                    offset = radius/2;
+                }   
+                var x = radius * Math.sin(Math.PI * 2 * this.data.angle / 360);
+                var z = ( radius * Math.cos(Math.PI * 2 *  this.data.angle / 360) ) * -1  ;
+                z +=  offset;
+                zoomedPos = [parseFloat(x).toFixed(3),this.data.position[1],parseFloat(z).toFixed(3)]
+                break;      
+        }   
 
-       /* this.el.setAttribute('animation__mouseenter', { property: 'position',
-                                            to: {x:this.el.object3D.position.x, y: this.el.object3D.position.y, z: 0.1},
+        this.el.setAttribute('animation__positionEnter', { property: 'position',
+                                            to: {x: zoomedPos[0], y: zoomedPos[1], z: zoomedPos[2]},
                                             dur: 150,
                                             startEvents: 'mouseenter'}); 
 
-        this.el.setAttribute('animation__mouseenter2', { property: 'scale',
-                                            to: {x:1.1, y:1.1, z: 1},
-                                            dur: 150,
-                                            startEvents: 'mouseenter'});  
-
-        this.el.setAttribute('animation__mouseleave', { property: 'position',
-                                            to: {x:this.el.object3D.position.x, y: this.el.object3D.position.y, z: 0},
+        this.el.setAttribute('animation__positionLeave', { property: 'position',
+                                            to: {x: this.data.position[0], y: this.data.position[1], z: this.data.position[2]},
                                             dur: 150,
                                             startEvents: 'mouseleave'});  
 
-        this.el.setAttribute('animation__mouseleave2', { property: 'scale',
-                                            to: {x:1, y: 1, z: 1},
-                                            dur: 150,
-                                            startEvents: 'mouseleave'});      */                              
+        // Scale animation
+        this.el.setAttribute('animation__scaleEnter', { property: 'scale',
+                to: {x:1.1, y:1.1, z: 1},
+                dur: 150,
+                startEvents: 'mouseenter'});                                      
+
+        this.el.setAttribute('animation__scaleLeave', { property: 'scale',
+                to: {x:1, y: 1, z: 1},
+                dur: 150,
+                startEvents: 'mouseleave'});   
+                               
     },
     events: {
         mouseenter: function (evt) {
