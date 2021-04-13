03b553c6ec4ab76043f214155ff85d236fa8afe8
CAVALLERO TRISTAN <tristan.cavallero@etu.unistra.fr>
2021-04-13 14:41:58 +0200

    [UPDATE] Add tha read table route

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,18 +1,32 @@
 export class ApiService {
 
-  static rCommandPOST(var_name, command_name, command_params) {
-    let full_command = var_name + " <- " + command_name + "(" + command_params + ")";
-    let data = { "command": full_command };
+    static rCommandPOST(var_name, command_name, command_params) {
+        let full_command = var_name + " <- " + command_name + "(" + command_params + ")";
+        let data = { "command": full_command };
 
-    fetch('http://localhost:3000/command', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      mode: 'cors',
-      headers: {
-        "Content-type": "application/json",
-      }
-    })
-    .then(json => console.log(json))
-    .catch(err => console.log(err));;
-  }
+        fetch('http://localhost:3000/command', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                mode: 'cors',
+                headers: {
+                    "Content-type": "application/json",
+                }
+            })
+            .then(json => console.log(json))
+            .catch(err => console.log(err));;
+    }
+
+    static rReadTableGET(var_name, page, step = 50) {
+        let full_url = 'http://localhost:3000/read/table?name=' + var_name + "&page=" + page + "&number=" + step;
+
+        fetch(full_url, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    "Content-type": "application/json",
+                }
+            })
+            .then(json => console.log(json))
+            .catch(err => console.log(err));;
+    }
 };
