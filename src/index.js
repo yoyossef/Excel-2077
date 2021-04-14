09d77c4635fbf43f1cfeeb1a91ba5ca984f8371c
Raphael Schwehr <raphael.schwehr@etu.unistra.fr>
2021-04-14 09:41:17 +0200

    merge

--- src/index.js
+++ src/index.js
@@ -7,12 +7,8 @@ import 'aframe-event-set-component';
 require('./controllers/ToolController.js');
 require('./services/ApiService.js');
 require('./controllers/TableController.js');
-<<<<<<< HEAD
 require('./controllers/CameraController.js');
 require('./components/cell_component/cell-component.js');
-=======
-require('./components/cell-component.js');
->>>>>>> develop
 require('./components/select-tool-component.js');
 require('./components/table-component.js');
 require('./components/toolsBelt_component/toolsBelt-component.js');
