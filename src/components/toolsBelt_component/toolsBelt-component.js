137272cb6adacb5a78a2d73098f861566d7040b1
Raphael Schwehr <raphael.schwehr@etu.unistra.fr>
2021-05-11 19:18:51 +0200

    ⚡️ toolbelt ticks only every 50ms (20 times per seconds instead of 90)

--- src/components/toolsBelt_component/toolsBelt-component.js
+++ src/components/toolsBelt_component/toolsBelt-component.js
@@ -10,11 +10,12 @@ AFRAME.registerComponent('tools-belt', {
 		// 	arc: '360'
 		// });
 		this.el.setAttribute('rotation', "-90 0 0");
+		this.tick = AFRAME.utils.throttleTick(this.tick, 50, this); //to only tick every 50ms (20 instead of 90 times per seconds)
 	},
 	tick: function () {
 		let camera = CameraController.getCamera();
 		let position = Object.assign({},camera.el.getAttribute('position')); //cloning camera position
-	    let rotation = camera.el.getAttribute('rotation');//cloning camera rotation
+		let rotation = camera.el.getAttribute('rotation');//cloning camera rotation
 
 		if (rotation.x >= -40)
 			this.el.setAttribute("visible", false);
