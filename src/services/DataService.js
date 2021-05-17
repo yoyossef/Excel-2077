4c4382048ddd56fd29e822d69d0898d88324efb6
CAVALLERO TRISTAN <tristan.cavallero@etu.unistra.fr>
2021-05-17 21:04:43 +0200

    :bug_fix: DataService using the wrong var_name when calling rReadTableGET function

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,6 +1,6 @@
-import {ApiService} from './ApiService.js';
-import {TableController} from '../controllers/TableController.js';
-import {config} from  '../.env.js';
+import { ApiService } from './ApiService.js';
+import { TableController } from '../controllers/TableController.js';
+import { config } from '../.env.js';
 
 export class DataService {
 
@@ -8,62 +8,59 @@ export class DataService {
     static nbCommandExecuted = 0;
     static displayedData = '';
 
-    static loadDataset(){
+    static loadDataset() {
         let tmpData = [];
         DataService.nbCommandExecuted++;
-        ApiService.rCommandPOST('c'+DataService.nbCommandExecuted,'read.table',"'"+config.DATASET+"', header=T, sep=','").then((response) => {
-                response.json().then((body) => {
-                    tmpData.push(body.columnsName);//headers
-                    setTimeout(function(){//Wait 100ms because R seems to crash if read is too quick
-                        ApiService.rReadTableGET('main',1).then((response) => {
-                            response.json().then((body) => {
-                                for(let i = 0; i<body.results.length;i++){
-                                    tmpData.push(Object.values(body.results[i]));
-                                }
-                                DataService.data['c'+DataService.nbCommandExecuted] = {
-                                        command: "read.table("+config.DATASET+", header=T, sep=',')",
-                                        table: tmpData,
-                                        totalResults: body.totalResults,
-                                        totalPages: body.totalPages
-                                    };
-                                TableController.loadDataInTable(DataService.data['c'+DataService.nbCommandExecuted].table);
-                                DataService.displayedData='c'+DataService.nbCommandExecuted;
-                            });
+        ApiService.rCommandPOST('c' + DataService.nbCommandExecuted, 'read.table', "'" + config.DATASET + "', header=T, sep=','").then((response) => {
+            response.json().then((body) => {
+                tmpData.push(body.columnsName); //headers
+                setTimeout(function() { //Wait 100ms because R seems to crash if read is too quick
+                    ApiService.rReadTableGET('c' + DataService.nbCommandExecuted, 1).then((response) => {
+                        response.json().then((body) => {
+                            for (let i = 0; i < body.results.length; i++) {
+                                tmpData.push(Object.values(body.results[i]));
+                            }
+                            DataService.data['c' + DataService.nbCommandExecuted] = {
+                                command: "read.table(" + config.DATASET + ", header=T, sep=',')",
+                                table: tmpData,
+                                totalResults: body.totalResults,
+                                totalPages: body.totalPages
+                            };
+                            TableController.loadDataInTable(DataService.data['c' + DataService.nbCommandExecuted].table);
+                            DataService.displayedData = 'c' + DataService.nbCommandExecuted;
                         });
-                    }, 100);
-                });
-            }
-        );
+                    });
+                }, 100);
+            });
+        });
     }
 
-
-    static executeCommand(commandName,params){
+    static executeCommand(commandName, params) {
         let tmpData = [];
         DataService.nbCommandExecuted++;
-        ApiService.rCommandPOST('c'+DataService.nbCommandExecuted,commandName,params).then((response) => {
-                response.json().then((body) => {
-                    tmpData.push(Object.keys(body.results[0]));//headers
-                    for(let i = 0; i<body.results.length;i++){
-                        tmpData.push(Object.values(body.results[i]));
-                    }
-                    DataService.data['c'+DataService.nbCommandExecuted] = {
-                            command: commandName+"("+params+")",
-                            table: tmpData,
-                            totalResults: body.totalResults,
-                            totalPages: body.totalPages
-                        };
-                    TableController.loadDataInTable(DataService.data['c'+DataService.nbCommandExecuted].table);
-                    DataService.displayedData='c'+DataService.nbCommandExecuted;
-                });
-            }
-        );
+        ApiService.rCommandPOST('c' + DataService.nbCommandExecuted, commandName, params).then((response) => {
+            response.json().then((body) => {
+                tmpData.push(Object.keys(body.results[0])); //headers
+                for (let i = 0; i < body.results.length; i++) {
+                    tmpData.push(Object.values(body.results[i]));
+                }
+                DataService.data['c' + DataService.nbCommandExecuted] = {
+                    command: commandName + "(" + params + ")",
+                    table: tmpData,
+                    totalResults: body.totalResults,
+                    totalPages: body.totalPages
+                };
+                TableController.loadDataInTable(DataService.data['c' + DataService.nbCommandExecuted].table);
+                DataService.displayedData = 'c' + DataService.nbCommandExecuted;
+            });
+        });
     }
 
-    static select(colIndexes){
+    static select(colIndexes) {
         let params = DataService.displayedData; //First param of select, the table name
-        for(let i =0;i<colIndexes.length;i++){//getting columns' name
-            params+=","+DataService.data[DataService.displayedData].table[0][colIndexes[i]];
+        for (let i = 0; i < colIndexes.length; i++) { //getting columns' name
+            params += "," + DataService.data[DataService.displayedData].table[0][colIndexes[i]];
         }
-        DataService.executeCommand('select',params);
+        DataService.executeCommand('select', params);
     }
 };
